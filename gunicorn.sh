d5c46f3eda85c74a5c8fa0ddc5508ed47a98f1a8
Kelvin <54338129+Breathing-ls-Fun@users.noreply.github.com>
2023-04-15 09:28:10 -0400

    gunicorn setup

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,35 +1,33 @@
-# #!/bin/bash
+#!/bin/bash
 
-# source env/bin/activate
+source env/bin/activate
 
-# cd /var/lib/jenkins/workspace/django-cicd/app
+cd /var/lib/jenkins/workspace/nf-app
 
-# python3 manage.py makemigrations
-# python3 manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 # python3 manage.py collectstatic -- no-input
 
-# echo "Migrations done"
+echo "Migrations done"
 
-# cd /var/lib/jenkins/workspace/django-cicd
+# cd /var/lib/jenkins/workspace/nf-app
 
-# sudo cp -rf gunicorn.socket /etc/systemd/system/
-# sudo cp -rf gunicorn.service /etc/systemd/system/
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
 
-# echo "$USER"
-# echo "$PWD"
+echo "$USER"
+echo "$PWD"
 
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
 
+echo "Gunicorn has started."
 
-# sudo systemctl daemon-reload
-# sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
 
-# echo "Gunicorn has started."
+echo "Gunicorn has been enabled."
 
-# sudo systemctl enable gunicorn
-
-# echo "Gunicorn has been enabled."
-
-# sudo systemctl restart gunicorn
+sudo systemctl restart gunicorn
 
 
-# sudo systemctl status gunicorn
+sudo systemctl status gunicorn
