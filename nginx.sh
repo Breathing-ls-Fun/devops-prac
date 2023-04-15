3c70ed46a7a8d784617a3b0db4a3dd19073791c0
Kelvin <54338129+Breathing-ls-Fun@users.noreply.github.com>
2023-04-15 10:13:52 -0400

    .

--- nginx.sh
+++ nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo cp -rf nf-app.conf /etc/nginx/sites-available/app
-chmod 710 /var/lib/jenkins/workspace/nf-app
+chmod 705 /var/lib/jenkins/workspace/nf-app
 
 sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
 sudo nginx -t
